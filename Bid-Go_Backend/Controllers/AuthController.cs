55402ee81811e5db76a5bd55b6c9552f05a152ab
Miguel Barbosa <miguelbarbosa2002@outlook.pt>
2025-10-20 13:45:15 +0100

    Endpoint para cada tipo de user

--- Bid-Go_Backend/Controllers/AuthController.cs
+++ Bid-Go_Backend/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Bid_Go_Backend.Data.Repositories.Interfaces;
 using Bid_Go_Backend.DTOs;
 using Bid_Go_Backend.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Bid_Go_Backend.Controllers
 {
@@ -46,5 +47,37 @@ namespace Bid_Go_Backend.Controllers
                 Expiration = DateTime.UtcNow.AddMinutes(60)
             });
         }
+        // Endpoint protegido com JWT
+        [HttpGet("me")]
+        [Authorize]
+        public IActionResult Me()
+        {
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+            var userId = User.Claims.FirstOrDefault(c => c.Type == "userId")?.Value;
+            var userType = User.Claims.FirstOrDefault(c => c.Type == "userType")?.Value;
+
+            return Ok(new
+            {
+                message = "Endpoint protegido",
+                email = userEmail,
+            });
+        }
+
+        // Apenas Companys podem aceder
+        [HttpGet("company-endpoint")]
+        [Authorize(Policy = "CompanyOnly")]
+        public IActionResult CompanyEndpoint()
+        {
+            return Ok(new { message = "Apenas Companies conseguem ver isto!" });
+        }
+
+        // Apenas Drivers podem aceder
+        [HttpGet("driver-endpoint")]
+        [Authorize(Policy = "DriverOnly")]
+        public IActionResult DriverEndpoint()
+        {
+            return Ok(new { message = "Apenas Drivers conseguem ver isto!" });
+        }
+
     }
 }
